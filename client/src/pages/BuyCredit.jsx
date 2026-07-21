3b8a9ea4954cd14fab8c4ade0c557fc1599e0d31
Ankit787762 <ankitsawami6@gmail.com>
2026-07-21 13:00:59 +0530

    Implement Razorpay payment verification endpoint and update credit balance on successful payment

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -21,7 +21,16 @@ const BuyCredit = () => {
     order_id: order.id,
     receipt:order.receipt,
     handler:async (response)=>{
-      console.log(response);
+      try {
+        const {data} = await axios.post(backendUrl+ '/api/user/verify-razor', response,{headers:{token}})
+        if(data.success){
+          loadCreditsData();
+          navigate('/')
+          toast.success('credit Added')
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
     }
    }
    const rzp = new window.Razorpay(options)
